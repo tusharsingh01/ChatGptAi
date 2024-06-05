de2682324d2bb6f221e05a57a3b39c46d83cf20f
tusharsingh01 <ts537685@gmail.com>
2024-06-05 12:49:09 +0530

    first commit

---
+++ chat/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Chatcomponent from "@/components/Chatcomponent";
+
+export default function Home() {
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <div className="bg-slate-800 p-3 w-[800px] rounded-md text-white">
+      <h2 className="text-2xl"> GPT-4 Streaming Chat Application</h2>
+      <Chatcomponent/>
+     </div>
+    </main>
+  );
+}
